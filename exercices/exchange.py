858a186ba7cc222d864319e99dfc8e095aa9018d
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2024-05-06 16:20:55 -0400

    Exercice 3 en version Python

---
+++ exercices/exchange.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from mpi4py import MPI  # MPI.Init() implicite
+import sys
+
+
+def main():
+    """
+    Programme principal
+    """
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    if size < 2:
+        sys.exit(None)
+
+    if rank == 0:
+        A = [1.0, 2.0]
+
+        # Send and Receive
+        comm.( ... )
+        comm.( ... )
+        print(f'Process {rank} sent     A=[{A[0]}, {A[1]}]')
+        print(f'Process {rank} received B=[{B[0]}, {B[1]}]')
+
+    if rank == 1:
+        B = [3.0, 4.0]
+
+        # Send and Receive
+        comm.( ... )
+        comm.( ... )
+        print(f'Process {rank} sent     B=[{B[0]}, {B[1]}]')
+        print(f'Process {rank} received A=[{A[0]}, {A[1]}]')
+
+    MPI.Finalize()
+
+
+if __name__ == '__main__':
+    main()
